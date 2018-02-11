e001b8b5646907a1061883d964f1097209cfcc41
Caique <caique.c.s@outlook.com>
2018-02-11 14:24:00 -0300

    projeto de frases

--- Frases do Dia/ViewController.swift
+++ Frases do Dia/ViewController.swift
@@ -15,6 +15,18 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBOutlet weak var labelFrase: UITextView!
+    @IBAction func gerarNovaFrase(_ sender: Any) {
+        var frases: [String] = []
+        
+        let aleatorio = arc4random_uniform(3)
+        frases.append("A new hope!");
+        frases.append("Oloco bixo");
+        frases.append("Hoje vai!");
+        
+        labelFrase.text = frases[ Int( aleatorio ) ]
+        
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
